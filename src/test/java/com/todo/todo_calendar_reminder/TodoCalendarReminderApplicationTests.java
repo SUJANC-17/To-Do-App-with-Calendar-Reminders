3e9ff889d6e671238f67f89da2a4cfbb319dd1ee
Sujan C <sujanc3306@gmail.com>
2026-06-25 12:17:03 +0530

    Initialized the local setup and commited it

---
+++ src/test/java/com/todo/todo_calendar_reminder/TodoCalendarReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.todo_calendar_reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoCalendarReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
